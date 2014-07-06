c0c7e3c470dabd9de054d98b9ad750805a29012c
Ben Zhang <nebgnahz@gmail.com>
2014-07-05 23:07:45 -0400

    cleaned up comments in main file

--- main.go
+++ main.go
@@ -1,14 +1,3 @@
-// ok, top down approach
-// this cli will take a few routes:
-// 1, new, when you register a website
-// 2, add, if you manually created the password and want it to be stored here
-// 3, set, you may update the value
-// 4, get, get the password
-// 5, rm, removes the entry
-// 6, ls, list what's stored
-// when we create the file, we should consider encrypt it with a master key. let's do this later
-
-// so far the following is only a hello world application that takes arguments...
 package main
 
 import (
